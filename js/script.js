00df0f179d243bf1b702d67f51ff5190eb1671ad
Astolin <rugal_diego@hotmail.com>
2022-06-26 01:04:26 -0700

    go

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log('Hello there... :D');
+const carouselContaienr = document.getElementById('carousel');
+
+for (let i = 2; i<31; i++) {
+    const div = document.createElement('div');
+    div.style.backgroundImage = `url(/img/carousel/${i+1}.jpg)`;
+    div.classList.add('carousel_item');
+    carouselContaienr.appendChild(div);
+}
+
+const div = document.createElement('div');
+div.style.backgroundImage = `url(/img/carousel/3.jpg)`;
+div.classList.add('carousel_item');
+carouselContaienr.appendChild(div);
